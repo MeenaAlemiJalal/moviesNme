ac08f4da328e6d87239f5c661e950d7e92205af6
Damediaz <diaz.damian2015@gmail.com>
2022-01-18 21:32:32 -0800

    updated watchlist

--- models/WatchList.js
+++ models/WatchList.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class WatchList extends Model {}
 
-Post.init(
+WatchList.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,20 +11,19 @@ Post.init(
       primaryKey: true,
       autoIncrement: true
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    movie_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'movie',
+        key: 'id'
+      }
     },
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    owner: {
+    user_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
-        key: 'id',
-      },
+        key: 'id'
+      }
     }
   },
   {
@@ -32,8 +31,8 @@ Post.init(
     timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post'
+    modelName: 'watchlist'
   }
 );
 
-module.exports = Post;
+module.exports = WatchList;
